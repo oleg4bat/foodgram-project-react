9be168cc49c4a1ddcff5b34c291b89801bf833c5
Олег Чеботарев <oleg4bat@gmail.com>
2023-06-09 00:44:30 +0300

    fix

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -7,9 +7,9 @@ class RecipeFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
                                              to_field_name='slug',
                                              queryset=Tag.objects.all())
-    is_favorited = filters.BooleanFilter(
+    is_favorited = filters.NumberFilter(
         method='is_favorited_filter')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_shopping_cart = filters.NumberFilter(
         method='is_in_shopping_cart_filter')
 
     class Meta:
@@ -19,13 +19,13 @@ class RecipeFilter(filters.FilterSet):
     def is_favorited_filter(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(favorite_recipe=user)
+            return queryset.filter(favorite_recipe__user=user)
         return queryset
 
     def is_in_shopping_cart_filter(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(shopping_recipe=user)
+            return queryset.filter(shopping_recipe__user=user)
         return queryset
 
 
