d6f567e36864d9adc0771086f4c53a1c8a483357
Олег Чеботарев <oleg4bat@gmail.com>
2023-06-08 12:53:21 +0300

    fix

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -80,7 +80,7 @@ class UserViewSet(mixins.CreateModelMixin,
 
         if request.method == 'POST':
             serializer = SubscribeAuthorSerializer(
-                author, data=request.data, context={"request": request})
+                author)
             serializer.is_valid(raise_exception=True)
             Subscribe.objects.create(user=request.user, author=author)
             return Response(serializer.data,
